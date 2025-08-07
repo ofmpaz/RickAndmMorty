0c0ae730bdec0cb778858b199aceb30e01e8e5a5
Deise Andrade <deise.paz@qintess.com>
2025-08-07 08:40:02 -0300

    Mudança das rotas GetMapping centralizando "Personagem"

--- deise/src/main/java/br/gov/ba/prodeb/deise/controller/PersonagemController.java
+++ deise/src/main/java/br/gov/ba/prodeb/deise/controller/PersonagemController.java
@@ -1,5 +1,6 @@
 package br.gov.ba.prodeb.deise.controller;
 
+import br.gov.ba.prodeb.deise.dto.EpisodeDTO;
 import br.gov.ba.prodeb.deise.dto.PersonagemDTO;
 import br.gov.ba.prodeb.deise.dto.PersonagemResponseDTO;
 import br.gov.ba.prodeb.deise.service.PersonagemService;
@@ -10,18 +11,18 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/personagem")
 public class PersonagemController {
 
     @Autowired
     private PersonagemService personagemService;
 
-    @GetMapping("/personagem")
-    public ResponseEntity<PersonagemResponseDTO> obterTodos() {
-        return ResponseEntity.ok(personagemService.obterTodos());
+    @GetMapping
+    public ResponseEntity<PersonagemResponseDTO> obterTodos(@RequestParam(required = false, defaultValue = "1") int page) {
+        return ResponseEntity.ok(personagemService.obterTodos(page));
     }
 
-    @GetMapping("/personagem/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<PersonagemDTO> obterPorId(@PathVariable int id) {
         return ResponseEntity.ok(personagemService.buscarPorId(id));
     }
@@ -35,5 +36,15 @@ public class PersonagemController {
     @GetMapping("/filtrar")
     public ResponseEntity<PersonagemResponseDTO> filtrar(@RequestParam String name) {
         return ResponseEntity.ok(personagemService.filtrar(name));
+    }
+
+    @GetMapping("/epsodios/{id}")
+    public ResponseEntity<EpisodeDTO> epsodiosComPersonagem(@PathVariable int id) {
+        return ResponseEntity.ok(personagemService.buscarEpsodios(id));
+    }
+
+    @GetMapping("/multiplos-epsodios")
+    public ResponseEntity<EpisodeDTO> buscarMultiplosEpsodios(@RequestParam String episode) {
+        return ResponseEntity.ok(personagemService.buscarMultiplosEpsodios(episode));
     }
 }
